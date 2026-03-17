76569830401de0c59289993e0a35cc84238489f7
accessmakr <accessmakr@gmail.com>
2026-03-17 04:13:13 +0100

    server.js
    updated

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+
+
+
 const express = require("express");
 const cors = require("cors");
 const ytdlp = require("youtube-dl-exec");
@@ -11,6 +14,8 @@ app.use(express.json());
 // serve index.html and other static files from the root folder
 app.use(express.static(__dirname));
 
+const cache = {};
+
 // simple test route so Railway homepage doesn't show "Cannot GET /"
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
@@ -20,14 +25,23 @@ app.get("/", (req, res) => {
 app.post("/api/info", async (req, res) => {
   const { url } = req.body;
 
-  if (!url) {
-    return res.status(400).json({ error: "URL required" });
+  if (!url || !/^https?:\/\//i.test(url)) {
+    return res.status(400).json({ error: "Valid URL required" });
   }
+
+  if (cache[url]) {
+    console.log("Cache hit for URL:", url);
+    return res.json(cache[url]);
+  }
+
+  console.log("Fetching video info for:", url);
 
   try {
     const info = await ytdlp(url, {
       dumpSingleJson: true,
-      noWarnings: true
+      noWarnings: true,
+      youtubeSkipDashManifest: true,
+      maxBuffer: 50 * 1024 * 1024
     });
 
     const formats = info.formats
@@ -38,15 +52,18 @@ app.post("/api/info", async (req, res) => {
         ext: f.ext
       }));
 
-    res.json({
+    const responseData = {
       title: info.title,
       thumbnail: info.thumbnail,
       duration: info.duration,
       formats
-    });
+    };
+
+    cache[url] = responseData;
+    res.json(responseData);
 
   } catch (err) {
-    console.error(err);
+    console.error("yt-dlp error:", err);
     res.status(500).json({ error: "Download failed" });
   }
 });
