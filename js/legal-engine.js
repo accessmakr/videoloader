962ec23b01446d238aadabeeb7cb13d51b625dee
accessmakr <accessmakr@gmail.com>
2026-03-19 12:45:11 +0100

    legal-engine.js

--- js/legal-engine.js
+++ js/legal-engine.js
@@ -5,6 +5,8 @@ window.addEventListener('load', () => {
   const legalPages = window.siteRegistry.filter(p => p.type === 'legal');
   const ul = document.getElementById('footer-menu');
   if (ul) {
-    ul.innerHTML = legalPages.map(p => `<li><a href="/${p.slug}" class="hover:text-white">${p.title}</a></li>`).join('');
+    ul.innerHTML = legalPages.map(p => 
+      `<li><a href="${p.slug || '/'}" class="hover:text-white transition-colors">${p.title}</a></li>`
+    ).join('');
   }
 });
